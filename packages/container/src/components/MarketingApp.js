b0032c60f32333af3b4cc20c476d1690a528f5dc
Aleksandar Beric <aleksandar.beric@htecgroup.com>
2022-09-17 11:11:08 +0200

    Add handle navigation in container and marketing app

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,25 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 
 export default () => {
     const marketingAppRef = useRef(null);
+    const history = useHistory(); // copy of our browser routing history in the container app
 
     useEffect(() => {
-        mount(marketingAppRef.current)
-    })
+        const { onParentNavigate } = mount(marketingAppRef.current, { //onParentNavigate is defined in the bootstrap.js of Marketing app
+            onNavigate: ({pathname: nextPathname}) => { // location object has pathname property, also we are renaming pathname to nextPathname
+                const { pathname } = history.location;
+
+                if(pathname !== nextPathname){ // we need this check to avoid infinite loop of redirecting
+                    history.push(nextPathname);
+                }
+            }
+        });
+
+        history.listen(onParentNavigate);
+    }, []) // only run this once when the component is rendered to the screen
 
     return <div ref={marketingAppRef} />
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App'
 
 //Mount func to start up the app
-const mount = (el) => { // el -> HTML element passed from development or prod/dev or prod/prod
+const mount = (el, { onNavigate, defaultHistory }) => { // el -> HTML element passed from development or prod/dev or prod/prod
+
+    const history = defaultHistory || createMemoryHistory(); //We are using this to handle proper routing for MFE architecture, default history is for stand alone app in dev environment
+
+    if(onNavigate) { // we need this check because we wont get onNavigate if we are starting marketing app as a standalone instead from the container app
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {  //we return this object so that container can get information from marketing app 
+        onParentNavigate({pathname: nextPathname}) { // location object has pathname property it history.listen returns this object this is from Container app
+            const { pathname } = history.location;
+            if(pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 }
 
 //if we are in development and in isolation,
@@ -16,7 +33,7 @@ if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root')
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() }); //mount expects second object which is navigate but we are not getting it in the standalone app and we are using browsing history in development mode
     }
 }
 
